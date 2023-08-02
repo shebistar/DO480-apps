411e5d712412024086741fe7cfc6eda4b0c3679d
Sebastian Rodriguez Isaziga (Extern) <Sebastian.Rodriguez.Isaziga.extern@signal-iduna.de>
2023-08-02 10:14:03 +0200

    Changes to adapt to SI infra

--- ansible/htpasswd-realm/prereq.sh
+++ ansible/htpasswd-realm/prereq.sh
@@ -1,4 +1,6 @@
 ansible-galaxy collection install community.okd
 ansible-galaxy collection install community.general
 ansible-galaxy collection install kubernetes.core
-
+pip3.8 install requests-oauthlib
+pip3.8 install passlib
+pip3.8 install kubernetes
